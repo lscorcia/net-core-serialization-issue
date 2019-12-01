0ab4035b858c18e404546f7f250aabef876a8ef0
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2019-12-01 16:58:04 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WsServiceProxy;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var authentication = new WSAuthentication();
+            var documentoProtocollato = new DocumentoProtocollato();
+
+            ProtocolloWebServiceClient wsProxy = new ProtocolloWebServiceClient();
+            var serviceResponse = wsProxy.protocolla(authentication, documentoProtocollato);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
